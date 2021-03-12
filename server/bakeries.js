c3622ed31e1180dff74ced5613c981a469c5737d
Tom <tommay90@gmail.com>
2021-03-12 15:31:03 +1300

    add bakeries routes

---
+++ server/bakeries.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const db = require('./db/bakeries')
+const router = express.Router
+
+router.get('/', (req,res) => {
+    db.getBakeries()
+        .then(bakeries => {
+            res.json(bakeries)
+        })
+        .catch(err => {
+            console.log("Error", err)
+            res.status(500).json({message: "Something went worng"})
+        })
+})
+
+router.get('/:id', (req,res) => {
+    const bakeryId = req.params.id
+    db.getBakeryById(bakeryId)
+    .then(bakery => {
+        res.json(bakery)
+    })
+    .catch(err => {
+        console.log("Error", err)
+        res.status(500).json({message: "Something went worng"})
+    })
+})
+
+router.post('/', (req,res) => {
+    const bakery = req.body
+    db.addBakery(bakery)
+    .then(() => {
+        res.sendStatus(200)
+    })
+    .catch(err => {
+        console.log("Error", err)
+        res.status(500).json({message: "Something went worng"})
+    })
+})
+
+router.patch('/:id', (req,res) => {
+    const id = req.params.id
+    const bakery = req.body
+    db.updateBakery(id, bakery.name, bakery.address)
+
+})
+
+router.delete('/:id', (req,res) => {
+    const id = req.params.id
+    db.deleteBakery(id)
+    .then(() => {
+        res.sendStatus(200)
+    })
+    .catch(err => {
+        console.log("Error", err)
+        res.status(500).json({message: "Something went worng"})
+    })
+})
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const path = require('path')
 
-const fruitRoutes = require('./routes/fruits')
+const bakeryRoutes = require('./routes/bakeries')
+const userRoutes = require('./routes/users')
+const reviewRoutes = require('./routes/reviews')
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.use('/api/v1/fruits', fruitRoutes)
+server.use('/api/v1/bakeries', bakeryRoutes)
+server.use('/api/v1/users', userRoutes)
+server.use('/api/v1/reviews', reviewRoutes)
 
 module.exports = server
