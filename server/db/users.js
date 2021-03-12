b2f78b2193922fe1915b8f1e22137c2ecef079d3
Tom <tommay90@gmail.com>
2021-03-13 11:15:22 +1300

    add user routes

--- server/db/users.js
+++ server/db/users.js
@@ -3,9 +3,22 @@ const connection = require('./connection')
 function addUsers (user, db = connection) {
   return db('users')
   .insert(user, 'id')
-  .then(ids => ids[0])
+  // .then(ids => ids[0])
+}
+
+function updateUser (id, firstName, lastName, email, location, db = connection) {
+  return db('users')
+  .where('id', id)
+  .update ({
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    location, location
+  })
+
 }
 
 module.exports = {
-  addUsers
+  addUsers,
+  updateUser
 }
---
+++ server/routes/users.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const db = require('../db/users')
+const router = express.Router()
+
+router.post('/', (req, res) => {
+    const user = req.body
+    db.addUsers(user)
+        .then(() => {
+            res.sendStatus(200)
+        })
+        .catch(err => {
+            console.log("Error", err)
+            res.status(500).json({message: "Something went worng"})
+        })
+})
+
+router.patch('/:id', (req,res) => {
+    const id = req.params.id
+    const user = req.body
+    db.updateUser(
+        id, 
+        user.firstName, 
+        user.lastName, 
+        user.email,
+        user.location
+        )
+        .then(() => {
+            res.sendStatus(200)
+        })
+        .catch(err => {
+            console.log("Error", err)
+            res.status(500).json({message: "Something went worng"})
+        })
+})
+
+
+module.exports = router
