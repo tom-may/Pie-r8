778853011e5890f37aca2e43f10553bee261df03
Tom <tommay90@gmail.com>
2021-03-17 11:28:55 +1300

    delete bakery functionality working

--- client/components/Bakeries.jsx
+++ client/components/Bakeries.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { fetchBakeries, deleteTheBakery } from '../actions/bakeries'
+import { addBakery } from '../apis/bakeries'
 
 
 function Bakeries(props){
@@ -10,16 +11,31 @@ function Bakeries(props){
         props.dispatch(fetchBakeries())
     }, [])
 
-    // const deleteBakery = (id) => {
-    //     return props.dispatch(deleteTheBakery(id))
-    // }
+    const handleDelete = (id, e) => {
+			if(confirm ("Are you sure you want to delete this bakery?")) {
+				e.preventDefault()
+        props.dispatch(deleteTheBakery(id))
+				alert("Deleted")
+				props.history.push("/bakeries")
+			}
+			else {
+				alert("Not deleted")
+			}
+    }
 
 console.log(props)
 
     return(
         <>
             {props.bakeries.map(bakery=> {
-                return <h1>{bakery.name}</h1>
+							const id = bakery.id
+                return( 
+                    <>
+                        <h1 key={bakery.id}>{bakery.name}</h1>
+												<button onClick={(e)=>handleDelete(id, e)}>Delete</button>
+												<br></br>
+                    </>
+                )
                 }
             )}
 
