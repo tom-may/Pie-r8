c5ead68f9fec896b61e91e350a73c04b1da7baaa
Tom <tommay90@gmail.com>
2021-03-17 10:37:11 +1300

    bakeries apis added

--- client/apis/bakeries.js
+++ client/apis/bakeries.js
@@ -8,3 +8,26 @@ export function getBakeries () {
       return res.body.bakeries
     })
 }
+
+export function getBakeryById(id) {
+  return request.get(rootUrl + '/' + id)
+  .then(res => res.body)
+}
+
+export function addBakery(bakery) {
+  return request.post(rootUrl)
+  .send(bakery)
+  .then(res => res.body)
+  }
+
+  export function deleteBakery(id) {
+    return request.delete(rootUrl + '/' + id)
+    .then(res => res.body)
+  }
+
+  export function updateBakery(id, bakery) {
+    return request.patch(rootUrl + '/' + id)
+    .send(bakery)
+    .then(res => res.body)
+  }
+  
