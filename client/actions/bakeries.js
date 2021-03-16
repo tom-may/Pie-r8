d31514deadaf6cd8a4e3df2ee571129f88c65d5d
Tom <tommay90@gmail.com>
2021-03-17 10:18:33 +1300

    bakery actions added

--- client/actions/bakeries.js
+++ client/actions/bakeries.js
@@ -1,6 +1,10 @@
-import { getBakeries } from '../apis/bakeries'
+import { getBakeries, getBakeryById, addBakery, deleteBakery, updateBakery } from '../apis/bakeries'
 
 export const SET_BAKERIES = 'SET_BAKERIES'
+export const SET_BAKERY_BY_ID = 'SET_BAKERY_BY_ID'
+// export const ADD_BAKERIES = 'ADD_BAKERIES'
+// export const DELETE_BAKERY = 'DELETE_BAKERY'
+// export const UPDATE_BAKERY = 'UPDATE_BAKERY'
 
 export function setBakeries (bakeries) {
   return {
@@ -9,6 +13,20 @@ export function setBakeries (bakeries) {
   }
 }
 
+export function setBakeryById (bakery) {
+  return {
+    type: SET_BAKERY_BY_ID,
+    bakery
+  }
+}
+
+// export function addBakeries (bakeries) {
+//   return {
+//     type: ADD_BAKERIES,
+//     bakeries
+//   }
+// }
+
 export function fetchBakeries () {
   return dispatch => {
     return getBakeries()
@@ -16,5 +34,44 @@ export function fetchBakeries () {
         dispatch(setBakeries(bakeries))
         return null
       })
+  }
+}
+
+export function fetchBakeryById () {
+  return dispatch => {
+    return getBakeryById (id)
+    .then(bakery => {
+      dispatch(setBakeryById(bakery))
+      return null
+    })
+  }
+}
+
+export function addTheBakery (bakery) {
+  return dispatch => {
+    return addBakery(bakery)
+    .then(() => {
+      dispatch(fetchBakeries())
+      return null
+    })
+  }
+}
+
+export function deleteTheBakery(id) {
+  return dispatch => {
+    return deleteBakery(id)
+    .then (() => {
+      dispatch(fetchBakeries())
+    })
+  }
+}
+
+export function updateTheBakery(id, bakery) {
+  return dispatch => {
+    return updateBakery(id, bakery)
+    .then(() => {
+      dispatch(fetchBakeries())
+      return null
+    })
   }
 }
--- client/reducers/bakeries.js
+++ client/reducers/bakeries.js
@@ -1,4 +1,4 @@
-import { SET_BAKERIES, ADD_BAKERY, DELETE_BAKERY, UPDATE_BAKERY } from '../actions/bakeries'
+import { SET_BAKERIES, ADD_BAKERIES, DELETE_BAKERY, UPDATE_BAKERY, SET_BAKERY_BY_ID } from '../actions/bakeries'
 
 const initialState = []
 
@@ -6,12 +6,14 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_BAKERIES:
       return action.bakeries
-    case ADD_BAKERY:
-      return [...state, {id: action.id, name: action.name, address: action.address}]
-    case DELETE_BAKERY:
-      return state.filter(bakery => bakery.id !== action.id)
-    case UPDATE_BAKERY:
-      return state.map(bakery => bakery.id === action.id)
+    case SET_BAKERY_BY_ID:
+      return  action.bakery
+    // case ADD_BAKERIES:
+    //   return [...state, {id: action.id, name: action.name, address: action.address}]
+    // case DELETE_BAKERY:
+    //   return state.filter(bakery => bakery.id !== action.id)
+    // case UPDATE_BAKERY:
+    //   return state.map(bakery => bakery.id === action.id)
     default:
       return state
   }
